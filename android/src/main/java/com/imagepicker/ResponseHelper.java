446e4005ebd32c99667235c55aa7a005657ede88
Peter Gaultney <pgaultney@xoi.io>
2019-12-14 16:05:32 -0600

    untabify?

--- android/src/main/java/com/imagepicker/ResponseHelper.java
+++ android/src/main/java/com/imagepicker/ResponseHelper.java
@@ -12,73 +12,73 @@ import com.facebook.react.bridge.WritableMap;
 
 public class ResponseHelper
 {
-	private WritableMap response = Arguments.createMap();
+    private WritableMap response = Arguments.createMap();
 
-	public void cleanResponse()
-	{
-		response = Arguments.createMap();
-	}
+    public void cleanResponse()
+    {
+        response = Arguments.createMap();
+    }
 
-	public @NonNull WritableMap getResponse()
-	{
-		return response;
-	}
+    public @NonNull WritableMap getResponse()
+    {
+        return response;
+    }
 
-	public void putString(@NonNull final String key,
-						  @NonNull final String value)
-	{
-		response.putString(key, value);
-	}
+    public void putString(@NonNull final String key,
+                          @NonNull final String value)
+    {
+        response.putString(key, value);
+    }
 
-	public void putInt(@NonNull final String key,
-					   final int value)
-	{
-		response.putInt(key, value);
-	}
+    public void putInt(@NonNull final String key,
+                       final int value)
+    {
+        response.putInt(key, value);
+    }
 
-	public void putBoolean(@NonNull final String key,
-						   final boolean value)
-	{
-		response.putBoolean(key, value);
-	}
+    public void putBoolean(@NonNull final String key,
+                           final boolean value)
+    {
+        response.putBoolean(key, value);
+    }
 
-	public void putDouble(@NonNull final String key,
-						  final double value)
-	{
-		response.putDouble(key, value);
-	}
+    public void putDouble(@NonNull final String key,
+                          final double value)
+    {
+        response.putDouble(key, value);
+    }
 
-	public void invokeCustomButton(@NonNull final Callback callback,
-								   @NonNull final String action)
-	{
-		cleanResponse();
-		response.putString("customButton", action);
-		invokeResponse(callback);
-	}
+    public void invokeCustomButton(@NonNull final Callback callback,
+                                   @NonNull final String action)
+    {
+        cleanResponse();
+        response.putString("customButton", action);
+        invokeResponse(callback);
+    }
 
-	public void invokeCancel(@NonNull final Callback callback)
-	{
-		cleanResponse();
-		response.putBoolean("didCancel", true);
-		if (callback != null) {
-			invokeResponse(callback);
-		}
-	}
+    public void invokeCancel(@NonNull final Callback callback)
+    {
+        cleanResponse();
+        response.putBoolean("didCancel", true);
+        if (callback != null) {
+            invokeResponse(callback);
+        }
+    }
 
-	public void invokeError(@NonNull final Callback callback,
-							@NonNull final String error)
-	{
-		cleanResponse();
-		response.putString("error", error);
-		if (callback != null) {
-			invokeResponse(callback);
-		}
-	}
+    public void invokeError(@NonNull final Callback callback,
+                            @NonNull final String error)
+    {
+        cleanResponse();
+        response.putString("error", error);
+        if (callback != null) {
+            invokeResponse(callback);
+        }
+    }
 
-	public void invokeResponse(@NonNull final Callback callback)
-	{
-		if (callback != null) {
-			callback.invoke(response);
-		}
-	}
+    public void invokeResponse(@NonNull final Callback callback)
+    {
+        if (callback != null) {
+            callback.invoke(response);
+        }
+    }
 }
